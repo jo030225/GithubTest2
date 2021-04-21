bada64add6f7ea727fb7c54333412e51a1e7f2b2
jo030225 <whwngur123@naver.com>
2021-04-21 15:37:38 +0900

    [UPDATE] 파일명 변경
    ViewController -> LoginViewController

--- GithubTest2/GithubTest2/LoginViewController.swift
+++ GithubTest2/GithubTest2/LoginViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  LoginViewController.swift
 //  GithubTest2
 //
 //  Created by 조주혁 on 2021/04/21.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
